08f48e1d88b4e755938b448d64df80ceb79e0b33
Иван Уваров <tonk2012@gmail.com>
2015-09-30 04:56:07 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# open-web.github.io
---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    maps = require('gulp-sourcemaps'),
+    livereload = require('gulp-livereload');
+
+
+// Compiles all Sass file into CSS (doesn't minify)
+gulp.task("compileSass", function() {
+    return gulp.src('scss/app.scss')
+        .pipe(maps.init())
+        .pipe(sass({ includePaths: ['./bower_components/foundation/scss'] }))
+        .pipe(maps.write("./"))
+        .pipe(gulp.dest('css'))
+        .pipe(livereload());
+});
+
+gulp.task("htmlUpdated", function() {
+    return gulp.src('index.html')
+    .pipe(livereload());
+});
+
+gulp.task('watch', function() {
+    livereload.listen();
+    gulp.watch("scss/**/*.scss", ['compileSass']);
+    gulp.watch("index.html", ['htmlUpdated']);
+});
+
+gulp.task('default', ['compileSass']);
